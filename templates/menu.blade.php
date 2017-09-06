bf39f0bf0e37945b750897b81384f670f6f466a2
Zhang Wenbin <zhangwb@shinetechchina.com>
2017-09-06 18:56:58 +0800

    updated menu icon

--- templates/menu.blade.php
+++ templates/menu.blade.php
@@ -1,3 +1,3 @@
 <router-link tag="li" to="/activity_log" exact>
-    <a><i class="fa fa-circle-o"></i> <span> @lang('module_activity_log::t.title')</span></a>
+    <a><i class="fa fa-history"></i> <span> @lang('module_activity_log::t.title')</span></a>
 </router-link>
